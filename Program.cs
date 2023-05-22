cfea8580ec08cef61e9d56bcd1d94a70ba23f7f5
andeuter <108286840+andeuter@users.noreply.github.com>
2023-05-22 18:36:36 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Taschenrechner_01
+{
+    /// <summary>
+    /// Erster einfacher Entwurf eines Taschenrechners,
+    /// der 2 Integer-Zahlen addiert
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implementieren der beiden Summanden
+            long a, b;
+
+            // Eingabe und Konvertierung der Summanden
+            Console.WriteLine("Bitte gib den ersten Summanden ein ...");
+            a = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("\nBitte gib den zweiten Summanden ein ...");
+            b = Convert.ToInt64(Console.ReadLine());
+
+            // Ausgabe der Addition
+            Console.WriteLine("\nDie Summe von {0} + {1} ist {2}.", a, b, (a + b));
+
+            // Programm beenden
+            Console.WriteLine("\nProgramm mit 'Enter' beenden ...");
+            Console.ReadKey();
+        }
+    }
+}
